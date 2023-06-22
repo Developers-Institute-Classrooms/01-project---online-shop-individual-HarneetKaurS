59037f910e6d8f025d65ae06ed18365c7bdf8167
Harneet <harneet.sandhu@developersinstitute.co.nz>
2023-06-22 14:38:17 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import Header from "./components/Header";
 import Products from "./components/Products";
 
 
+
 // The function that makes the fetch request to the Products API
 import { getProducts } from "./services/getProducts";
 
-function App() {
+
+ function App() {
   // use the products variable to read all of your products
   // and display them on your page
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const loadData = async () => {
+ 
+ useEffect(() => {
+   const loadData = async () => {
       const products = await getProducts();
+     
      // console.log(products);
-      setProducts(products);
+     setProducts(products);
     };
 
-    loadData();
+   loadData();
   }, []);
 
   return (
@@ -27,6 +31,7 @@ function App() {
      
     < Header />
     <h1> Welcome to Travel Accessories store</h1>
+    
     <Products products={products} />
     </div>
   
