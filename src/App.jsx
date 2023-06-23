878d0e0c5a362fc0f45ba1f7ad55932fed44fed0
Harneet <harneet.sandhu@developersinstitute.co.nz>
2023-06-22 17:55:23 -0700

    Add filterby in App file

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,35 @@ import { React, useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Products from "./components/Products";
+import FilterBy from "./components/FilterBy";
+
+
 
 
 
 // The function that makes the fetch request to the Products API
 import { getProducts } from "./services/getProducts";
 
-
- function App() {
+import {filterByCategory} from "./services/filterByCategory"; 
+ 
+function App() {
   // use the products variable to read all of your products
   // and display them on your page
   const [products, setProducts] = useState([]);
+const [filteredProducts, setFilteredProducts]= useState([]);
+
+const onFilterProducts = (filterBy) =>
+setFilteredProducts(filterByCategory(products, filterBy));
 
  
  useEffect(() => {
    const loadData = async () => {
       const products = await getProducts();
      
-     // console.log(products);
+      console.log(products);
      setProducts(products);
+     setFilteredProducts(products);
+   
     };
 
    loadData();
@@ -31,7 +41,7 @@ import { getProducts } from "./services/getProducts";
      
     < Header />
     <h1> Welcome to Travel Accessories store</h1>
-    
+    <FilterBy setFilterBy={onFilterProducts} />
     <Products products={products} />
     </div>
   
